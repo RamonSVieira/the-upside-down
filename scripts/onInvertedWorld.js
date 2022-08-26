81e6ee7ad9b965e82b6520c7c4093d5718beea68
Ramon Vieira <ramonvieira122333@gmail.com>
2022-08-26 14:11:04 -0300

    Embedding the audio according to the theme

--- scripts/onInvertedWorld.js
+++ scripts/onInvertedWorld.js
@@ -3,6 +3,12 @@ const theme = document.querySelector("body");
 
 button.addEventListener("click", changeTheme);
 
+window.addEventListener("click", function () {
+  const audio = document.getElementById("music");
+  audio.play();
+  audio.volume = 0.2;
+});
+
 function changeTheme() {
   if (theme.classList.contains("light-theme")) {
     theme.classList.add("dark-theme");
@@ -11,4 +17,15 @@ function changeTheme() {
     theme.classList.add("light-theme");
     theme.classList.remove("dark-theme");
   }
+
+  const themeMusic = document.body.classList[0];
+  const music =
+    themeMusic === "light-theme"
+      ? "assets_musics_normal-world.mpeg"
+      : "inverted-world.mpeg";
+
+  const audio = document.getElementById("music");
+  audio.src = `./musics/${music}`;
+  audio.play();
+  audio.volume = 0.2;
 }
